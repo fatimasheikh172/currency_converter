1578bb9e5953e3425e30e2007086d1593100507f
githubusername <githubemail>
2024-07-03 00:40:43 +0500

    This  is a curency converter machine

---
+++ index.ts
@@ -0,0 +1,48 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const currency: any = {
+     USD: 1,
+     EUR: 0.91,
+     GBR: 0.76,
+     INR: 74.57,
+     PKR: 280
+};
+
+let user_answer = await inquirer.prompt(
+    [
+        {
+            name: "from",
+            message: "Enter from currency",
+            type: "list",
+            choices: ["USD","EUR","GBR","INR","PKR"]
+                
+        },
+
+        {
+            name: "to",
+            message: "Enter to currency",
+            type: "list",
+            choices: ["USD","EUR","GBR","INR","PKR"]
+        },
+
+        {
+            name: "amount",
+            message: "Enter your amount",
+            type: "number"
+        }
+    ]
+);
+
+let fromAmount = currency[user_answer.from];
+
+let toAmount =  currency[user_answer.to];
+
+let amount = user_answer.amount;
+
+let newamount = amount/fromAmount;
+
+let converteramount = newamount * toAmount
+
+console.log(Math.round(converteramount))
